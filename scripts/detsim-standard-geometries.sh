62a0397f6e0efb9a165412fde3ffa8ca115ff780
Clark McGrew <clark.mcgrew@stonybrook.edu>
2017-01-03 09:30:14 -0500

    Remove CAPTAIN scripts that won't be part of edep-sim.

--- scripts/detsim-standard-geometries.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-#  This generates the standard set of geometry files for use in the
-#  reconstruction.
-# 
-set -e
-
-${DETSIMROOT}/scripts/detsim-export-geometry.sh baseline 1.0
-${DETSIMROOT}/scripts/detsim-export-geometry.sh mini-captain 1.0
