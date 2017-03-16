8e28c39ffdf759b4b04c09a2f499dec35d84bbe5
Clark McGrew <clark.mcgrew@stonybrook.edu>
2017-03-16 17:05:22 -0400

    Fix a bad typo in the running of edep-sim.

--- test/testTree.sh
+++ test/testTree.sh
@@ -8,8 +8,7 @@ if [ -d edepsimTree ]; then
     rm -rf edepsimTree
 fi
 
-edep-sim -o edepsimTree.root -e 10 -u 
-	 ${EDEP_ROOT}/inputs/muon-10000.mac \
+edep-sim -o edepsimTree.root -e 10 -u ${EDEP_ROOT}/inputs/muon-10000.mac 
 
 root -l -q edepsimTree.root ${EDEP_ROOT}/tools/loadEDepSim.C readTree.C
 
