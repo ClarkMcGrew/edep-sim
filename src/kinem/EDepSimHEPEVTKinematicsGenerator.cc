3483bb59554afc6a68d258383a30b6278643e8fe
Clark McGrew <clark.mcgrew@stonybrook.edu>
2021-09-06 12:01:29 -0400

    Implement custom HEPEVT file reader
    No longer use the G4HEPEvtInterface class to read the input file.  The
    custom reader checks the syntax of the input file and adds (hopefully)
    useful error messages.  The current behavior is to terminate when an
    input error is found.

--- src/kinem/EDepSimHEPEVTKinematicsGenerator.cc
+++ src/kinem/EDepSimHEPEVTKinematicsGenerator.cc
@@ -1,35 +1,190 @@
-#include <G4Event.hh>
-#include <G4HEPEvtInterface.hh>
-
+#include "kinem/EDepSimHEPEVTKinematicsGenerator.hh"
 #include "EDepSimException.hh"
 
-#include "kinem/EDepSimHEPEVTKinematicsGenerator.hh"
+
+#include <G4Event.hh>
+#include <G4SystemOfUnits.hh>
+#include <G4LorentzVector.hh>
+#include <G4PrimaryVertex.hh>
+
+#ifdef USE_G4HEPEvtInterface
+#include <G4HEPEvtInterface.hh>
+#endif
+
+#include <memory>
 
 EDepSim::HEPEVTKinematicsGenerator::HEPEVTKinematicsGenerator(
     const G4String& name, const G4String& fileName, int verbosity)
-    : EDepSim::VKinematicsGenerator(name), fGenerator(NULL) {
-    EDepSimLog("Open HEPEVT file " << fileName);
-    fGenerator = new G4HEPEvtInterface(fileName,verbosity);
-}
+    : EDepSim::VKinematicsGenerator(name), fGenerator(NULL),
+      fFileName(fileName), fVerbosity(verbosity) { }
 
 EDepSim::HEPEVTKinematicsGenerator::~HEPEVTKinematicsGenerator() {
+#ifdef USE_G4HEPEvtInterface
     if (fGenerator) delete fGenerator;
+#endif
+}
+
+int EDepSim::HEPEVTKinematicsGenerator::GetTokens(
+    std::vector<std::string>& tokens) {
+
+    tokens.clear();
+    if (!fInput.is_open() || fInput.eof()) {
+        EDepSimError("No more events: " << fFileName);
+        return 0;
+    }
+
+    std::string line;
+    while (std::getline(fInput,line)) {
+        ++fCurrentLine;
+        // Strip out comments.
+        line = line.substr(0,line.find_first_of("#"));
+        // Break into tokens.
+        std::istringstream input(line);
+        std::string token;
+        tokens.clear();
+        while (input >> token) tokens.push_back(token);
+        if (!tokens.empty()) return tokens.size();
+    }
+
+    EDepSimLog("No more data in " << fFileName);
+    return 0;
+}
+
+int EDepSim::HEPEVTKinematicsGenerator::AsInteger(const std::string& token) {
+    std::istringstream input(token);
+    int value;
+    input >> value;
+    std::string trailer;
+    if (input >> trailer) {
+        EDepSimThrow(fFileName << ":" << fCurrentLine
+                     << " -- Invalid integer: |" << token<< "|");
+    }
+    return value;
+}
+
+double EDepSim::HEPEVTKinematicsGenerator::AsReal(const std::string& token) {
+    std::istringstream input(token);
+    double value;
+    input >> value;
+    std::string trailer;
+    if (input >> trailer) {
+        EDepSimThrow(fFileName << ":" << fCurrentLine
+                     << " -- Invalid real: |" << token << "|");
+    }
+    return value;
 }
 
 EDepSim::VKinematicsGenerator::GeneratorStatus
 EDepSim::HEPEVTKinematicsGenerator::GeneratePrimaryVertex(
     G4Event* evt, const G4LorentzVector& /* position */) {
+#ifdef USE_G4HEPEvtInterface
     if (!fGenerator) {
-        EDepSimThrow("EDepSim::HEPEVTKinematicsGenerator::"
-                     " File Not Open");
+        fGenerator = new G4HEPEvtInterface(fFileName,fVerbosity);
     }
     try {
         fGenerator->GeneratePrimaryVertex(evt);
     }
     catch (...) {
-        EDepSimError("Kaboom");
-        exit(1);
+        EDepSimError("No more events.");
+        throw EDepSim::NoMoreEvents();
     }
+#else
+    if (!fInput.is_open()) {
+        fInput.open(fFileName,std::ifstream::in);
+        fCurrentLine = 0;
+        if (!fInput.is_open()) {
+            EDepSimThrow("File not open: " << fFileName);
+        }
+        EDepSimLog("Open HEPEVT file: " << fFileName);
+    }
+
+    std::vector<std::string> tokens;
+    int lines = 0;
+    double vtxX = 0;
+    double vtxY = 0;
+    double vtxZ = 0;
+    double vtxT = 0;
+
+    // Parse the first line of an event.  It can have either 1 integer
+    // (i.e. the standard syntax for the file), or 1 integer and 4 reals (my
+    // special extension).  The 4 reals are the position of the vertex in
+    // cm (xyz) and ns (t).
+    if (!GetTokens(tokens)) {
+        EDepSimLog("No more events in " << fFileName
+                   << " after " << fCurrentLine << " lines");
+        throw EDepSim::NoMoreEvents();
+    }
+    switch (tokens.size()) {
+    case 1:
+        lines = AsInteger(tokens[0]);
+        break;
+    case 5:
+        vtxX = AsReal(tokens[1])*cm;
+        vtxY = AsReal(tokens[2])*cm;
+        vtxZ = AsReal(tokens[3])*cm;
+        vtxT = AsReal(tokens[4])*ns;
+        lines = AsInteger(tokens[0]);
+        break;
+    default:
+        EDepSimError("Syntax error at " << fFileName << ":" << fCurrentLine);
+        throw EDepSim::NoMoreEvents();
+    }
+
+    if (fVerbosity>0) {
+        EDepSimNamedLog("HEPEVT", "Reading " << lines << " particles");
+    }
+    if (fVerbosity>1) {
+        EDepSim::LogManager::IncreaseIndentation();
+        EDepSimNamedLog("HEPEVT",
+                        "Vertex "
+                        << vtxX << " mm, "
+                        << vtxY << " mm, "
+                        << vtxZ << " mm, "
+                        << vtxT << " ns" );
+        EDepSim::LogManager::DecreaseIndentation();
+    }
+
+    std::unique_ptr<G4PrimaryVertex> vertex(
+        new G4PrimaryVertex(vtxX,vtxY,vtxZ,vtxT));
+
+    // Parse the particle lines for the event.  This loses some of the context
+    // information for now and only adds the particles that should be tracked.
+    for (int line = 0; line < lines; ++line) {
+        if (!GetTokens(tokens)) {
+            EDepSimError("Input truncated in " << fFileName
+                       << " after " << fCurrentLine << " lines");
+            throw EDepSim::NoMoreEvents();
+        }
+        if (tokens.size() != 8) {
+            EDepSimError("Invalid input at " << fFileName
+                         << ":" << fCurrentLine
+                         << " -- Wrong number of entries on line");
+            throw EDepSim::NoMoreEvents();
+        }
+        int status = AsInteger(tokens[0]);
+        if (status != 1) continue;
+        int pid = AsInteger(tokens[1]);
+        // int daugter1 = AsInteger(tokens[2]);  // ignored, here for docu.
+        // int daughter2 = AsInteger(tokens[3]); // ignored, here for docu.
+        double momX = AsReal(tokens[4])*GeV;
+        double momY = AsReal(tokens[5])*GeV;
+        double momZ = AsReal(tokens[6])*GeV;
+        // double mass = AsReal(tokens[7])*GeV;  // ignored, here for docu.
+        std::unique_ptr<G4PrimaryParticle> part(
+            new G4PrimaryParticle(pid, momX, momY, momZ));
+        vertex->SetPrimary(part.release());
+        if (fVerbosity>1) {
+            EDepSim::LogManager::IncreaseIndentation();
+            EDepSimNamedLog("HEPEVT",
+                            "Primary "
+                            << pid << " " << momX << " MeV/c, "
+                            << momY << " MeV/c, "
+                            << momZ << " MeV/c" );
+            EDepSim::LogManager::DecreaseIndentation();
+        }
+    }
+    evt->AddPrimaryVertex(vertex.release());
+#endif
 
     return kSuccess;
 }
--- src/kinem/EDepSimHEPEVTKinematicsGenerator.hh
+++ src/kinem/EDepSimHEPEVTKinematicsGenerator.hh
@@ -6,12 +6,14 @@
 
 #include "kinem/EDepSimVKinematicsGenerator.hh"
 
+#include <string>
+#include <fstream>
+
 class G4Event;
 class G4VPrimaryGenerator;
 
-/// A base class for primary generator classes used in the EDepSim::.  This is
-/// used by the EDepSim::UserPrimaryGeneratorAction to generate particles which
-/// will be tracked by the G4 simulation.
+/// A class to read a HEPEVT file.  This normally uses an internal processor,
+/// but can be forced (using #defines) to use the G4HEPEvtInterface.
 namespace EDepSim {class HEPEVTKinematicsGenerator;}
 class EDepSim::HEPEVTKinematicsGenerator :
     public EDepSim::VKinematicsGenerator {
@@ -27,6 +29,37 @@ public:
                           const G4LorentzVector& position);
 
 private:
+    /// The primary generator when G4HEPEvtInterface is used (usually NULL).
     G4VPrimaryGenerator* fGenerator;
+
+    /// The name of the input file (used for error messages)
+    std::string fFileName;
+
+    /// The number of lines read from the input file (used for error messages)
+    int fCurrentLine;
+
+    /// The verbosity of the output
+    int fVerbosity;
+
+    /// The input stream to be read.
+    std::ifstream fInput;
+
+    /// Get the next line of "tokens" from the input file.  This returns the
+    /// number of tokens that were read from the line, and zero when the file
+    /// is empty.  Comments in the file are prefixed by "#", and tokens are
+    /// separated by white space.  The expected format is
+    /// \code
+    /// token1 token2 token3 and so on # comments
+    /// \endcode
+    /// Lines that are empty, or only have comments are skipped,
+    int GetTokens(std::vector<std::string>& tokens);
+
+    /// Parse a token as an integer.  This will throw an error if the string
+    /// is not a valid integer.
+    int AsInteger(const std::string& token);
+
+    /// Parse a token as a real number This will throw an error if the string
+    /// is not a valid floating point number.
+    double AsReal(const std::string& token);
 };
 #endif
