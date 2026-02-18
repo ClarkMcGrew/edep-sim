2ef6601a5ecd52d0feae17e4d3360443931f4aec
Clark McGrew <clark.mcgrew@stonybrook.edu>
2026-02-18 16:16:01 -0500

    Run the stepping action to catch stuck trajectories

--- src/EDepSimCreateRunManager.cc
+++ src/EDepSimCreateRunManager.cc
@@ -4,6 +4,8 @@
 #include "EDepSimUserEventAction.hh"
 #include "EDepSimUserStackingAction.hh"
 #include "EDepSimUserTrackingAction.hh"
+#include "EDepSimUserSteppingAction.hh"
+
 #include "EDepSimDetectorMessenger.hh"
 
 #include "EDepSimCreateRunManager.hh"
@@ -16,21 +18,24 @@ G4RunManager* EDepSim::CreateRunManager(G4String physicsList) {
 
     // Construct the default run manager
     G4RunManager* runManager = new G4RunManager;
-    
+
     // Construct the detector construction class.
     EDepSim::UserDetectorConstruction* theDetector
         = new EDepSim::UserDetectorConstruction;
     runManager->SetUserInitialization(theDetector);
-    
+
     // Add the physics list first.  This is a G4 requirement!
     runManager->SetUserInitialization(new EDepSim::PhysicsList(physicsList));
-    
+
     // Set the other mandatory user action class
     runManager->SetUserAction(new EDepSim::UserPrimaryGeneratorAction);
     runManager->SetUserAction(new EDepSim::UserRunAction);
     runManager->SetUserAction(new EDepSim::UserEventAction);
     runManager->SetUserAction(new EDepSim::UserStackingAction);
     runManager->SetUserAction(new EDepSim::UserTrackingAction);
-    
+
+    // Add a break for problems.
+    runManager->SetUserAction(new EDepSim::SteppingAction);
+
     return runManager;
 }
