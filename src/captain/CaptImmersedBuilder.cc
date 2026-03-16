57de5cf91316412091eb1ed311e76c95d15bbe1c
Clark McGrew <clark.mcgrew@stonybrook.edu>
2026-03-16 15:27:59 -0400

    Add a boundary SD for the field cage
    This make the field cage into a fake photo sensor for testing.

--- src/captain/CaptImmersedBuilder.cc
+++ src/captain/CaptImmersedBuilder.cc
@@ -115,6 +115,9 @@ G4LogicalVolume *CaptImmersedBuilder::GetPiece(void) {
                               FindMaterial("FR4_Copper"),
                               GetName()+"/FieldCage");
     logFieldCage->SetVisAttributes(GetColor(logFieldCage));
+    EDepSim::SDFactory wallFactory("surface");
+    G4VSensitiveDetector* sensitiveWall = wallFactory.MakeSD("Photons");
+    logFieldCage->SetSensitiveDetector(sensitiveWall);
 
     EDepSimError("Build optical surface");
     // Make the surface of the field cage photo-sensitive
