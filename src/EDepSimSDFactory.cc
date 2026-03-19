437d34fe51a3269d87a94c8b3c931b7c1ae486d6
Clark McGrew <clark.mcgrew@stonybrook.edu>
2026-03-19 10:51:22 -0400

    Extend SDFactory interface for multiple SD types
    Adds optional type parameters to the GetSD and MakeSD methods.  The
    constructor still takes a type which provides a default when the type
    is not provided in GetSD and MakeSD.

--- src/EDepSimSDFactory.cc
+++ src/EDepSimSDFactory.cc
@@ -8,24 +8,48 @@
 EDepSim::SDFactory::SDFactory(G4String type)
     : fType(type) {}
 
-
 EDepSim::SDFactory::~SDFactory() {}
 
-G4VSensitiveDetector* EDepSim::SDFactory::GetSD(G4String name) {
-    return G4SDManager::GetSDMpointer()->FindSensitiveDetector(name);
+G4VSensitiveDetector* EDepSim::SDFactory::GetSD(G4String name, G4String type) {
+    G4VSensitiveDetector* sd
+        = G4SDManager::GetSDMpointer()->FindSensitiveDetector(name);
+    // Check that the type is correct
+    if (type == "undefined") {
+        type = fType;
+    }
+    if (type =="segment") {
+        EDepSim::SegmentSD* seg = dynamic_cast<EDepSim::SegmentSD*>(sd);
+        if (sd != nullptr && seg == nullptr) {
+            EDepSimError("SD " << name << " is not a SegmentSD");
+        }
+    }
+    else if (type == "surface") {
+        EDepSim::SurfaceSD* sur = dynamic_cast<EDepSim::SurfaceSD*>(sd);
+        if (sd != nullptr && sur == nullptr) {
+            EDepSimError("SD " << name << " is not a SurfaceSD");
+        }
+    }
+    else if (type != "undefined") {
+        EDepSimError("SD " << type << " is an unrecognized type");
+    }
+
+    return sd;
 }
 
-G4VSensitiveDetector* EDepSim::SDFactory::MakeSD(G4String name) {
-    G4VSensitiveDetector* sd = GetSD(name);
+G4VSensitiveDetector* EDepSim::SDFactory::MakeSD(G4String name, G4String type) {
+    G4VSensitiveDetector* sd = GetSD(name,type);
     if (!sd) {
-        if (fType == "segment") {
+        if (type == "undefined") {
+            type = fType;
+        }
+        if (type == "segment") {
             sd = new EDepSim::SegmentSD(name);
         }
-        else if (fType == "surface") {
+        else if (type == "surface") {
             sd = new EDepSim::SurfaceSD(name);
         }
         else {
-            EDepSimError("No such sensitive detector " << name);
+            EDepSimError("No such sensitive detector type:" << name);
             EDepSimThrow("sensitive detector type not implemented");
         }
         G4SDManager::GetSDMpointer()->AddNewDetector(sd);
